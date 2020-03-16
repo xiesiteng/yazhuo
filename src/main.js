e1dc6324f0c74c3a633f177b028c956ea30e781c
xiesiteng <goku_97@163.com>
2020-03-16 16:51:00 +0800

    xst

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import {router} from '@/router/index.js'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
+// +++
 
 Vue.use(VueAwesomeSwiper)
 
